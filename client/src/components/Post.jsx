dbffc6b787674c9f38d11162788c1a20682ccb74
khurshid idrees <umarkhurshid3@gmail.com>
2024-05-30 09:09:31 +0530

    update

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Post() {
  
@@ -29,7 +30,7 @@ export default function Post() {
                 Nutrition
               </a>
               <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
-                {post.title}
+                title
               </h2>
               <div className="flex mt-3">
                 <img
